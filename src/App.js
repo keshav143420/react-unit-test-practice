24829b79942f81c01e8196336a2dfc341a927678
keshava kotri <keshav143420@gmail.com>
2019-08-13 09:58:58 +0530

    added Header test and calss

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-          <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <Header />
       <p>
         Edit <code>src/App.js</code> and save to reload.
       </p>
