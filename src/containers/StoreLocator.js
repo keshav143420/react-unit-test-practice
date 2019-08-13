8b9f17e4470f26f45c6d016201e5528ad90fbd1e
keshava kotri <keshav143420@gmail.com>
2019-08-13 12:10:43 +0530

    added more test cases to StoreLocator

--- src/containers/StoreLocator.js
+++ src/containers/StoreLocator.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
+import Button from '../components/Button';
+import Map from '../components/Map';
 
 class StoreLocator extends Component {
   render(){
-    return("");
+    return(
+      <div>
+        <Header />
+        <Button />
+        <Button />
+        <Map />
+      </div>
+    );
   }
 }
 export default StoreLocator;
--- src/containers/__tests__/StoreLocator.test.js
+++ src/containers/__tests__/StoreLocator.test.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import StoreLocator from '../StoreLocator';
+import { exportAllDeclaration } from '@babel/types';
 
-it('renders without crashing', () => {
-  let mountedStoreLocator = shallow(<StoreLocator />);
+describe("StoreLocator",function(){
+  let mountedStoreLocator;
+  beforeEach(()=>{
+    mountedStoreLocator = shallow(<StoreLocator />);
+  })
+  
+  it('renders without crashing', () => {
+    let mountedStoreLocator = shallow(<StoreLocator />);
+  });
+
+  it('renders a header', () => {
+    const headers = mountedStoreLocator.find('Header');
+    expect(headers.length).toBe(1);
+  });
+
+  it('renders buttons', () => {
+    const buttons = mountedStoreLocator.find('Button');
+    expect(buttons.length).toBe(2);
+  });
+
+  it('renders a map', () => {
+    const maps = mountedStoreLocator.find('Map');
+    expect(maps.length).toBe(1);
+  });
+
 });
