04670556e18925df37ef0903c67a3341e3ff983c
keshava kotri <keshav143420@gmail.com>
2019-08-13 09:41:25 +0530

    added logo

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello World</h1>
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
