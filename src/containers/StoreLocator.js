beb1cbc6969b843e7bce355a43705650ddc9864f
keshava kotri <keshav143420@gmail.com>
2019-08-14 11:41:55 +0530

    Added buttons array

--- src/containers/StoreLocator.js
+++ src/containers/StoreLocator.js
@@ -4,15 +4,35 @@ import Button from '../components/Button';
 import Map from '../components/Map';
 
 class StoreLocator extends Component {
+  constructor(props) {
+    super(props);
+    this.shops = [
+      {
+        'location': 'Portland',
+        'address': '123 Portland Dr'
+      },
+      {
+        'location': 'Astoria',
+        'address': '123 toria Dr'
+      },
+      {
+        'location': '',
+        'address': ''
+      }
+    ]
+  }
+
   render() {
+
+    let storeButtons = this.shops.map((shop, id) => {
+      return (<Button key={id} location={shop.location} />);
+    })
     return (
 
       <div>
         <Header />
         <div>
-          <Button location='portland' />
-          <Button location='Astoria' />
-          <Button />
+          {storeButtons}
         </div>
         <Map />
       </div>
